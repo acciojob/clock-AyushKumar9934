97b39b3212ddda9ca9cee2f55f5bee87a9f47ea2
AyushKumar9934 <83179865+AyushKumar9934@users.noreply.github.com>
2024-04-10 18:24:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+let ptag=document.getElementById("timer");
+
+let current=new Date();
+function fn(){
+	ptag.innerHTML=current.toLocaleString();
+}
+
+
+
+
+setInterval(fn(),1000);
